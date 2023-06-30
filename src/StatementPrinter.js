90be4e4cb58af63a28815be767e35f93db65b7f8
alx-askw <alexw.askew77@gmail.com>
2023-06-30 18:35:58 +0100

    refactoring statementprinter

--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -1,23 +1,29 @@
-const { dodgerblue } = require("color-name");
 
 class StatementPrinter {
     static #printingStrings = [];
+    static #statementHeader = `date${"".padEnd(11 - 4)}|| credit  || debit  || balance`;
+
     static statementHandler(transArray) {
         for (let i = 0; i < transArray.length; i++) {
             StatementPrinter.#printingStrings.push(StatementPrinter.#stringFormatter(transArray[i][0], transArray[i][1]))
         }
-        return StatementPrinter.#printingStrings;
+        return StatementPrinter.#statementFinal();
     }
 
     static #stringFormatter(string, amount) {
-        let formattedString;
         if (string.getType() === 'withdraw') {
-            formattedString = `${string.getDate()} ||` + ` ${''.padEnd(7)} ||` + ` ${string.getAmount().toFixed(2)}${''.padEnd(8 - amount.toFixed(2).length)}|| ${amount.toFixed(2)}`
+            return `${string.getDate()} ||` + ` ${''.padEnd(7)} ||` + ` ${string.getAmount().toFixed(2)}${''.padEnd(8 - amount.toFixed(2).length)}|| ${amount.toFixed(2)}\n`;
         }
         if (string.getType() === 'deposit') {
-            formattedString = `${string.getDate()} ||` + ` ${string.getAmount().toFixed(2)}${''.padEnd(7 - amount.toFixed(2).length)} ||` + `${''.padEnd(7)} || ${amount.toFixed(2)}`
+            return `${string.getDate()} ||` + ` ${string.getAmount().toFixed(2)}${''.padEnd(7 - amount.toFixed(2).length)} ||` + `${''.padEnd(7)} || ${amount.toFixed(2)}\n`;
         }
-        return formattedString;
+    }
+
+    static #statementFinal(arrayOfTransactions) {
+        let finalStatement = `${StatementPrinter.#statementHeader}\n`
+        StatementPrinter.#printingStrings.forEach(line => { finalStatement += line });
+        return finalStatement;
+
     }
 
 
